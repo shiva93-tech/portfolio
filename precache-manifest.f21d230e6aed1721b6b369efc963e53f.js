a5cc82348cfe168cb28f302dd1eb9718b1895425
Shiv Narayana Kumar <shiva.kr0993@gmail.com>
2020-04-30 12:14:56 +0530

    Updates

---
+++ precache-manifest.f21d230e6aed1721b6b369efc963e53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1dfe28785442c906ffa4872a501a3ea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1618f461026e992f0fa4",
+    "url": "/portfolio/static/css/main.c40fcd5e.chunk.css"
+  },
+  {
+    "revision": "24fd4e6a8b4da13cf884",
+    "url": "/portfolio/static/js/2.b3aa6d2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b3aa6d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1618f461026e992f0fa4",
+    "url": "/portfolio/static/js/main.a1e923bb.chunk.js"
+  },
+  {
+    "revision": "a7113cebf7c686597c8b",
+    "url": "/portfolio/static/js/runtime-main.1924990b.js"
+  }
+]);
